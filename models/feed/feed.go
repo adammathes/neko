598c6c9ef39500926337e52967fd527833981909
Adam Mathes <adam@trenchant.org>
2017-11-22 10:38:24 -0700

    sort feeds by lc(title)

--- models/feed/feed.go
+++ models/feed/feed.go
@@ -25,7 +25,7 @@ func NewFeed(url string) error {
 }
 
 func All() ([]*Feed, error) {
-	return filter(" ORDER BY URL desc")
+	return filter(" ORDER BY lower(TITLE) asc")
 }
 
 func filter(where string) ([]*Feed, error) {
